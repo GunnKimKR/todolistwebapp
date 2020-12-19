13a2db8af3bfccc23da270cf02997512ddcb3200
br-yu <boram2286@gmail.com>
2020-12-20 05:42:30 +0900

    feat: add spinner, code refactoring

---
+++ src/front/src/components/common/Popup.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="popup" class="pop-wrap">
+    <MessagePopup
+      v-if="popup.name == 'message'"
+      @closePopup="closePopup"
+    ></MessagePopup>
+    <ResetPasswordPopup
+      v-if="popup.name == 'resetPassword'"
+      slot="pop-notice-content"
+      @closePopup="closePopup"
+    ></ResetPasswordPopup>
+  </div>
+</template>
+
+<script>
+import MessagePopup from '@/components/popup/MessagePopup.vue';
+import ResetPasswordPopup from '@/components/popup/ResetPasswordPopup.vue';
+export default {
+  components: {
+    MessagePopup,
+    ResetPasswordPopup,
+  },
+  computed: {
+    popup() {
+      return this.$store.state.popup;
+    },
+  },
+  methods: {
+    closePopup() {
+      this.$store.commit('closePopup');
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/front/src/components/common/Spinner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="spinner" v-if="isLoading">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['isLoading'],
+};
+</script>
+
+<style scoped>
+@import '~styles/spinner.css';
+</style>
