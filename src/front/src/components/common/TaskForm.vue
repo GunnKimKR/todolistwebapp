0603f2fae33a07668f590829dd836559d4a6abbd
Kim Geon <62473507+KimGeonkr@users.noreply.github.com>
2020-12-29 18:44:32 +0900

    docs: Add task form component

---
+++ src/front/src/components/common/TaskForm.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="task-form-container" v-bind:class="{ active: isActive }">
+    <h2 class="blind">Task form</h2>
+    <button
+      type="button"
+      class="task-input-btn"
+      role="Task input button"
+      v-bind:class="{ active: isActive }"
+      @click="taskFormShow"
+    ></button>
+    <div class="task-form__row">
+      <div class="input-box">
+        <input type="text" placeholder="+ Add a new task" />
+      </div>
+      <button type="button">OK</button>
+    </div>
+    <div class="task-form__row">
+      <label class="checkbox-box" for="task-date">
+        <input type="checkbox" id="task-date" />
+        <div class="checkbox"></div>
+        Date
+      </label>
+      <div class="task-details">
+        Sep 21. 2020 / 17:00 - 21:00
+      </div>
+    </div>
+    <div class="task-form__row task-recur">
+      <label class="checkbox-box" for="task-recurring">
+        <input type="checkbox" id="task-recurring" />
+        <div class="checkbox"></div>
+        Recurring
+      </label>
+      <div class="task-details">
+        3 Cycle
+      </div>
+    </div>
+    <div class="task-form__row">
+      <label class="checkbox-box" for="task-label-color">
+        <input type="checkbox" id="task-label-color" />
+        <div class="checkbox"></div>
+        Label Color
+      </label>
+      <div class="task-details">
+        <div class="label-square label--green"></div>
+      </div>
+    </div>
+    <div class="task-form__row">
+      <label class="checkbox-box" for="task-pin">
+        <input type="checkbox" id="task-pin" />
+        <div class="checkbox"></div>
+        Pin
+      </label>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+  methods: {
+    taskFormShow() {
+      this.isActive = !this.isActive;
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import '~styles/taskform.css';
+</style>
--- src/front/src/views/MainPage.vue
+++ src/front/src/views/MainPage.vue
@@ -2,28 +2,31 @@
   <div id="wrap">
     <NavMenu></NavMenu>
     <div id="container">
+      <TaskForm></TaskForm>
       <Calendar></Calendar>
       <ClipPlans></ClipPlans>
     </div>
-    <ClipPlanDetail></ClipPlanDetail>
+    <!-- <ClipPlanDetail></ClipPlanDetail> -->
     <MyInfo></MyInfo>
   </div>
 </template>
 
 <script>
 import NavMenu from '@/components/common/NavMenu.vue';
+import TaskForm from '@/components/common/TaskForm.vue';
 import MyInfo from '@/components/common/MyInfo.vue';
 import Calendar from '@/components/common/Calendar.vue';
 import ClipPlans from '@/components/main/ClipPlans.vue';
-import ClipPlanDetail from '@/components/main/ClipPlanDetail.vue';
+// import ClipPlanDetail from '@/components/main/ClipPlanDetail.vue';
 
 export default {
   components: {
     NavMenu,
+    TaskForm,
     MyInfo,
     Calendar,
     ClipPlans,
-    ClipPlanDetail,
+    // ClipPlanDetail,
   },
 };
 </script>
